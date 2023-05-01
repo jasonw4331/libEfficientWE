fe6e814aba3de52f93d2cb1b94e3e01cb21af84f
jasonwynn10 <jasonwynn10@me.com>
2023-05-01 04:39:03 -0400

    Sphere refactor

--- src/libEfficientWE/shapes/Sphere.php
+++ src/libEfficientWE/shapes/Sphere.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace libEfficientWE\shapes;
@@ -14,6 +15,10 @@ use pocketmine\world\format\SubChunk;
 use pocketmine\world\utils\SubChunkExplorer;
 use pocketmine\world\utils\SubChunkExplorerStatus;
 use pocketmine\world\World;
+use function abs;
+use function max;
+use function microtime;
+use function min;
 
 /**
  * A representation of a sphere shape.
@@ -38,40 +43,40 @@ class Sphere extends Shape {
 	}
 
 	public static function fromVector3(Vector3 $min, Vector3 $max) : Shape {
-		$minX = (int)min($min->x, $max->x);
-		$minY = (int)min($min->y, $max->y);
-		$minZ = (int)min($min->z, $max->z);
-		$maxX = (int)max($min->x, $max->x);
-		$maxY = (int)max($min->y, $max->y);
-		$maxZ = (int)max($min->z, $max->z);
+		$minX = min($min->x, $max->x);
+		$minY = min($min->y, $max->y);
+		$minZ = min($min->z, $max->z);
+		$maxX = max($min->x, $max->x);
+		$maxY = max($min->y, $max->y);
+		$maxZ = max($min->z, $max->z);
 
-		$center = new Vector3(($maxX + $minX) / 2, ($maxY + $minY) / 2, ($maxZ + $minZ) / 2);
-		$radius = ($maxX - $minX) / 2;
+		$center = new Vector3($minX + $maxX / 2, $minY + $maxY / 2, $minZ + $maxZ / 2);
+		$radius = $maxY - $minY / 2;
 		return new self($center, $radius);
 	}
 
 	public static function fromAABB(AxisAlignedBB $alignedBB) : Shape {
-		$minX = (int)min($alignedBB->minX, $alignedBB->maxX);
-		$minY = (int)min($alignedBB->minY, $alignedBB->maxY);
-		$minZ = (int)min($alignedBB->minZ, $alignedBB->maxZ);
-		$maxX = (int)max($alignedBB->minX, $alignedBB->maxX);
-		$maxY = (int)max($alignedBB->minY, $alignedBB->maxY);
-		$maxZ = (int)max($alignedBB->minZ, $alignedBB->maxZ);
+		$minX = min($alignedBB->minX, $alignedBB->maxX);
+		$minY = min($alignedBB->minY, $alignedBB->maxY);
+		$minZ = min($alignedBB->minZ, $alignedBB->maxZ);
+		$maxX = max($alignedBB->minX, $alignedBB->maxX);
+		$maxY = max($alignedBB->minY, $alignedBB->maxY);
+		$maxZ = max($alignedBB->minZ, $alignedBB->maxZ);
 
-		$center = new Vector3(($maxX + $minX) / 2, ($maxY + $minY) / 2, ($maxZ + $minZ) / 2);
-		$radius = ($maxX - $minX) / 2;
+		$center = new Vector3($minX + $maxX / 2, $minY + $maxY / 2, $minZ + $maxZ / 2);
+		$radius = $maxY - $minY / 2;
 		return new self($center, $radius);
 	}
 
-	public function copy(World $world, Vector3 $relativeCenter) : void{
-		$absoluteBasePos = $this->relativeCenter->subtractVector($relativeCenter->floor());
+	public function copy(World $world, Vector3 $worldPos) : void{
+		$absoluteBasePos = $this->center->subtractVector($worldPos->floor());
 
-		$relativeMaximums = $this->relativeCenter->add($this->radius, $this->radius, $this->radius);
+		$relativeMaximums = $this->center->add($this->radius, $this->radius, $this->radius);
 		$xCap = $relativeMaximums->x;
 		$yCap = $relativeMaximums->y;
 		$zCap = $relativeMaximums->z;
 
-		$relativeMinimums = $this->relativeCenter->subtract($this->radius, $this->radius, $this->radius);
+		$relativeMinimums = $this->center->subtract($this->radius, $this->radius, $this->radius);
 		$minX = $relativeMinimums->x;
 		$minY = $relativeMinimums->y;
 		$minZ = $relativeMinimums->z;
@@ -82,12 +87,12 @@ class Sphere extends Shape {
 
 		// loop from min to max if coordinate is in cylinder, save fullblockId
 		for($x = 0; $x <= $xCap; ++$x) {
-			$ax = $minX + $x;
+			$ax = (int) floor($minX + $x);
 			for($z = 0; $z <= $zCap; ++$z) {
-				$az = $minZ + $z;
+				$az = (int) floor($minZ + $z);
 				for($y = 0; $y <= $yCap; ++$y) {
-					$ay = $minY + $y;
-					if($this->relativeCenter->distance(new Vector3($x, $y, $z)) <= $this->radius && $subChunkExplorer->moveTo((int) $ax, (int) $ay, (int) $az) !== SubChunkExplorerStatus::INVALID) {
+					$ay = (int) floor($minY + $y);
+					if($this->center->distance(new Vector3($x, $y, $z)) <= $this->radius && $subChunkExplorer->moveTo($ax, $ay, $az) !== SubChunkExplorerStatus::INVALID) {
 						$blocks[World::blockHash($ax, $ay, $az)] = $subChunkExplorer->currentSubChunk?->getFullBlock($ax & SubChunk::COORD_MASK, $ay & SubChunk::COORD_MASK, $az & SubChunk::COORD_MASK);
 					}
 				}
@@ -97,7 +102,7 @@ class Sphere extends Shape {
 		$this->clipboard->setFullBlocks($blocks)->setRelativePos($absoluteBasePos)->setCapVector($relativeMaximums);
 	}
 
-	public function paste(World $world, Vector3 $relativePos, bool $replaceAir, ?PromiseResolver $resolver = null) : Promise{
+	public function paste(World $world, Vector3 $worldPos, bool $replaceAir, ?PromiseResolver $resolver = null) : Promise{
 		$time = microtime(true);
 		$resolver ??= new PromiseResolver();
 
@@ -110,7 +115,7 @@ class Sphere extends Shape {
 			$centerChunk,
 			$adjacentChunks,
 			$this->clipboard,
-			$relativePos,
+			$worldPos,
 			$this->radius,
 			true,
 			$replaceAir,
