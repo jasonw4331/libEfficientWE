71e575fd25527ffefdb817ced310767388a45d60
jasonwynn10 <jasonwynn10@me.com>
2023-05-02 07:58:48 -0400

    small var name refactor

--- src/libEfficientWE/task/copy/SphereCopyTask.php
+++ src/libEfficientWE/task/copy/SphereCopyTask.php
@@ -26,7 +26,7 @@ final class SphereCopyTask extends ChunksCopyTask{
 	 * @inheritDoc
 	 */
 	protected function readBlocks(SimpleChunkManager $manager, Vector3 $worldPos) : array{
-		$maxVector = $worldPos->add($this->radius * 2, $this->radius * 2, $this->radius * 2);
+		$worldMax = $worldPos->add($this->radius * 2, $this->radius * 2, $this->radius * 2);
 
 		/** @var array<BlockPosHash, int|null> $blocks */
 		$blocks = [];
@@ -35,11 +35,11 @@ final class SphereCopyTask extends ChunksCopyTask{
 		$center = new Vector3($this->radius, $this->radius, $this->radius);
 
 		// loop from 0 to max. if coordinate is in sphere, save fullblockId
-		for($x = 0; $x <= $maxVector->x; ++$x){
+		for($x = 0; $x <= $worldMax->x; ++$x){
 			$ax = (int) floor($worldPos->x + $x);
-			for($z = 0; $z <= $maxVector->z; ++$z){
+			for($z = 0; $z <= $worldMax->z; ++$z){
 				$az = (int) floor($worldPos->z + $z);
-				for($y = 0; $y <= $maxVector->y; ++$y){
+				for($y = 0; $y <= $worldMax->y; ++$y){
 					$ay = (int) floor($worldPos->y + $y);
 					if($center->distanceSquared(new Vector3($x, $y, $z)) <= $this->radius ** 2 && $subChunkExplorer->moveTo($ax, $ay, $az) !== SubChunkExplorerStatus::INVALID){
 						$blocks[morton3d_encode($x, $y, $z)] = $subChunkExplorer->currentSubChunk?->getFullBlock($ax & SubChunk::COORD_MASK, $ay & SubChunk::COORD_MASK, $az & SubChunk::COORD_MASK);
