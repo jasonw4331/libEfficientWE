be63e09cbcbc986f24e76738bf4fca4cfd224a64
jasonwynn10 <jasonwynn10@me.com>
2023-05-08 05:56:42 -0400

    new baseline

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -2,9 +2,39 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
-	'message' => '#^Parameter \\#5 \\$clipboard of method libEfficientWE\\\\task\\\\ChunksChangeTask\\:\\:setBlocks\\(\\) expects libEfficientWE\\\\utils\\\\Clipboard, mixed given\\.$#',
+	'message' => '#^Parameter \\#1 \\$value of method pocketmine\\\\promise\\\\PromiseResolver\\<array\\<string, array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>\\|float\\|int\\>\\>\\:\\:resolve\\(\\) expects array\\{chunks\\: array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>, time\\: float, blockCount\\: int\\}, array\\{chunks\\: array\\<int, pocketmine\\\\world\\\\format\\\\Chunk\\|null\\>, time\\: float, blockCount\\: int\\<0, max\\>\\} given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/libEfficientWE/shapes/Cone.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$value of method pocketmine\\\\promise\\\\PromiseResolver\\<array\\<string, array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>\\|float\\|int\\>\\>\\:\\:resolve\\(\\) expects array\\{chunks\\: array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>, time\\: float, blockCount\\: int\\}, array\\{chunks\\: array\\<int, pocketmine\\\\world\\\\format\\\\Chunk\\|null\\>, time\\: float, blockCount\\: int\\<0, max\\>\\} given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/libEfficientWE/shapes/Cuboid.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$value of method pocketmine\\\\promise\\\\PromiseResolver\\<array\\<string, array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>\\|float\\|int\\>\\>\\:\\:resolve\\(\\) expects array\\{chunks\\: array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>, time\\: float, blockCount\\: int\\}, array\\{chunks\\: array\\<int, pocketmine\\\\world\\\\format\\\\Chunk\\|null\\>, time\\: float, blockCount\\: int\\<0, max\\>\\} given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/libEfficientWE/shapes/Cylinder.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Result of && is always false\\.$#',
 	'count' => 2,
-	'path' => __DIR__ . '/src/libEfficientWE/task/ChunksChangeTask.php',
+	'path' => __DIR__ . '/src/libEfficientWE/shapes/Cylinder.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Strict comparison using \\!\\=\\= between 1 and 1 will always evaluate to false\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/src/libEfficientWE/shapes/Cylinder.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$value of method pocketmine\\\\promise\\\\PromiseResolver\\<array\\<string, array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>\\|float\\|int\\>\\>\\:\\:resolve\\(\\) expects array\\{chunks\\: array\\<pocketmine\\\\world\\\\format\\\\Chunk\\>, time\\: float, blockCount\\: int\\}, array\\{chunks\\: array\\<int, pocketmine\\\\world\\\\format\\\\Chunk\\|null\\>, time\\: float, blockCount\\: int\\<0, max\\>\\} given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/libEfficientWE/shapes/Sphere.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Match expression does not handle remaining values\\: int\\<min, \\-1\\>\\|int\\<3, max\\>$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/libEfficientWE/task/write/CylinderTask.php',
 ];
 
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
